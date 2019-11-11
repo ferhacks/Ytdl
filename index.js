7a6178cf64d4e15c1ab514ce48866d76d3957a5b
Norman <41760554+n0rmancodes@users.noreply.github.com>
2019-11-10 22:55:54 -0500

    spacing

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ console.log("starting up...")
 
 http.createServer(function (req, res) { 
 	console.log("got request!")
+	
 	if (!url.parse(req.url,true).search) {
 		res.writeHead(404, {"Content-Type": "text/plain"});
 		res.end("404: no url found");
@@ -14,6 +15,7 @@ http.createServer(function (req, res) {
 		var dUrl = url.parse(req.url,true).search.substring(5,999);
 		console.log("parsed url: " + dUrl);
 	}
+	
 	ytdl(dUrl, function(err, info) {
 		var json = JSON.stringify ({
 			datainfo: info
@@ -22,4 +24,5 @@ http.createServer(function (req, res) {
 		res.end(json);
 	})
 }).listen(3000);
+
 console.log("listening...")
